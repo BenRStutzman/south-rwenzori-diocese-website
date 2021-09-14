40e637ce5b8615cef0aeed598d81783f37eaaef6
BenRStutzman <tysorx89@gmail.com>
2021-09-14 12:51:54 +0300

    Add to bishop page

--- bishop/index.php
+++ bishop/index.php
@@ -10,7 +10,15 @@
 <body>
     <?php include "{$_SERVER['DOCUMENT_ROOT']}/shared/_header.php"; ?>
     <main>
-        <h1>Meet our Bishop</h1>
+        <h1>Meet our Bishop: The Rt. Rev. Nason Baluku</h1>
+        <section>
+            <p>South Rwenzori Diocese P.O.Box 142 Kasese +256 782 309 778 srdiocese@gmail.com</p>
+            <p>nasonbaluku2013@gmail.com</p>
+            <p>The Rt. Rev. Nason Baluku is the 3rd Bishop of the Diocese of South Rwenzori. He was consecrated and enthroned as Bishop on 13th December 2020 at St. Paul’s Cathedral, Kasese. He succeed the Rt. Rev. Jackson Nzerebende, who has been the Bishop of the Diocese since 2003.</p>
+            <p>Rt. Rev. Nason Baluku was born 15th March 1967 in in Mahango Sub-County on the slopes of the Rwenzori Mountains in Kasese district. He holds a Master of Arts in Organizational Leadership and Management and a Bachelor of Community Leadership and Development, both from Uganda Christian University. He received a Diploma in Theology from Uganda Christian University and a Provincial Certificate in Theological Studies from Bishop Balya Theological College.</p>
+            <p>He was born again at the age of 12 on 13th October 1979 and was confirmed three years later in 1982 by Bishop Kamanyire. By the time he was 17 years old, he was commissioned as a Catechist, and later a Lay Reader. On 6th August 1995, he was ordained a deacon and made a priest on 28th July 1996. He was appointed as a Canon of St. Paul’s Cathedral on 4th December 2005.</p>
+            <p>The Bishop has served in a variety of ministry positions, including a Vicar of several parishes, an Archdeacon, Diocesan Secretary, Program Director for the Bishop Masereka Christian Foundation, and PDR Coordinator in the Provincial Secretariat and a Project Manager for the international organization Barnabas Fund. He is married to Jane Biira Baluku and God has blessed them with seven children.</p>
+        </section>
         <section>
             <p>The Bishop’s office is overseen by Bishop of the South Rwenzori Diocese, with day-to-day affairs managed by the Diocesan Secretary and a management team of Head of the Departments and Units and the Archdeacons.</p>
             <p>The Diocesan Secretary is Rev. Mugisa Nason.</p>
