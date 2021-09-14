3db1a3f8548b925fa480afc9df8c9345b1614e79
BenRStutzman <tysorx89@gmail.com>
2021-09-14 11:03:30 +0300

    started adding bishop page

--- bishop/index.php
+++ bishop/index.php
@@ -12,6 +12,27 @@
     <main>
         <h1>Meet our Bishop</h1>
         <section>
+            <p>The Bishop’s office is overseen by Bishop of the South Rwenzori Diocese, with day-to-day affairs managed by the Diocesan Secretary and a management team of Head of the Departments and Units and the Archdeacons.</p>
+            <p>The Diocesan Secretary is Rev. Mugisa Nason.</p>
+            <p>It is the duty of the Bishop’s office to ensure the implementation of resolutions adopted by the Diocesan SYOND.</p>
+            <p>The Bishop’s office has five departments through which the coordination of ministry throughout all the Archdeaconries and Parishes is handled. The Departments are:</p>
+            <ul>
+                <li>Mission and Evangelism</li>
+                <li>Education</li>
+                <li>Health</li>
+                <li>Planning and development</li>
+                <li>Finance and administration</li>
+            </ul>
+            <p>The following programs and projects currently being run by the Bishop’s office:</p>
+            <ul>
+                <li>The Orphan and Vulnerable Children (OVC) project which supports  342 children with school fees and scholastic materials.</li>
+                <li>Clergy and Lay Reader’s SACCO which provide loans and retirement</li>
+                <li>A discipleship movement is rolling out targeting every church member to be a disciple who makes more disciples.</li>
+                <li>MBB’s</li>
+
+Prayer point
+
+· The Enthronement of Rt Rev Dr Kaziimba Mugalu as the 9th Archbishop on 1st March 2020, his Ministry and the retirement of The Most Rev Stanley Ntagali.
         </section>
     </main>
     <?php include "{$_SERVER['DOCUMENT_ROOT']}/shared/_footer.php"; ?>
