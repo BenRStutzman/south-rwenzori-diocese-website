6fb1acb22ce821439c86ddf2ce1a67732776a497
BenRStutzman <tysorx89@gmail.com>
2021-09-16 11:00:20 +0300

    add favicons

--- shared/_headTags.php
+++ shared/_headTags.php
@@ -6,13 +6,18 @@
     ?>
 </title>
 
-<!-- Fonts -->
+<!-- fonts -->
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Glory:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
 
-<!-- Css -->
+<!-- css -->
 <link rel="stylesheet" href="/css/site.css">
 
-<!-- So media queries work on mobile devices --->
+<!-- so media queries work on mobile devices --->
 <meta name="viewport" content="width=device-width,initial-scale=1">
+
+<!-- favicons -->
+<link rel="icon" type="image/png" sizes="16x16" href="/images/icons/favicon-16x16.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/images/icons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="/images/icons/favicon-96x96.png">
