4a01730d0e9eed4fbdb451efe023272582e7794c
BenRStutzman <tysorx89@gmail.com>
2021-09-17 16:10:02 +0300

    fill in initial pages

--- directorates/health/index.php
+++ directorates/health/index.php
@@ -13,24 +13,30 @@
     </div>
     <main>
         <section>
-            <p>Contact person: Rev Can. Josephat Bwalhuma, revjbwalhuma@gmail.com</p>
-            <p>Tel: 0772-694010</p>
-            <p>Diocesan Health and Social service Coordinator.</p>
-            <p>The Health Department exists to improve peoples’ health through quality care and effective service delivery.</p>
-            <p>South Rwenzori Diocese currently operates 1 hospital which Kagando Hospital and 8 Health Canters. Of which 2 are health center IVs, 6 are health center IIIs and 14 First Aid Centers.</p>
-            <p>South Rwenzori Diocese hospital and health clinics, together with our Roman Catholic counterparts, provide more than half the medical care offered in Kasese.</p>
+            <h2>Improving lives through quality healthcare</h2>
+            <p>The Health Department exists to improve people's health through quality care and effective service delivery.</p>
+            <p>South Rwenzori Diocese currently operates Kagando Hospital and 8 health centers, which include (2 health center IVs and 6 health center IIIs), and 14 First Aid Centers.</p>
+            <p>South Rwenzori Diocese hospital and health clinics, together with our Roman Catholic counterparts, provide more than half of the medical care offered in Kasese.</p>
             <p>South Rwenzori Diocese Health centers:</p>
-                <ol>
-                    <li>St. Paul Health Centre IV</li>
-                    <li>Rwensande Health Centre IV</li>
-                    <li>Maliba Health Centre III</li>
-                    <li>Kanamba Health Centre III</li>
-                    <li>Nyabugando Health Centre III</li>
-                    <li>Kinyamaseke Health Centre III</li>
-                    <li>Buhaghura Health Centre III</li>
-                    <li>Musenene Health Centre III</li>
-                </ol>
-            <p>The Health Department works collaboratively with the Uganda Protestant Medical Bureau (UPMB)</p>
+            <ol>
+                <li>St. Paul Health Centre IV</li>
+                <li>Rwensande Health Centre IV</li>
+                <li>Maliba Health Centre III</li>
+                <li>Kanamba Health Centre III</li>
+                <li>Nyabugando Health Centre III</li>
+                <li>Kinyamaseke Health Centre III</li>
+                <li>Buhaghura Health Centre III</li>
+                <li>Musenene Health Centre III</li>
+            </ol>
+            <p>The Health Department works collaboratively with the Uganda Protestant Medical Bureau (UPMB).</p>
+        <section>
+            <h2>Contact Information:</h2>
+            <p>Rev Can. Josephat Bwalhuma, Diocesan Health and Social service Coordinator
+            <br>
+            +256 772 694 010
+            <br>
+            <a href="mailto:revjbwalhuma@gmail.com">revjbwalhuma@gmail.com</a>
+            </p>
         </section>
     </main>
     <?php include "{$_SERVER['DOCUMENT_ROOT']}/shared/_footer.php"; ?>
