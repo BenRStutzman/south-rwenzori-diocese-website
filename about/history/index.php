36e8d224f74618fd77e3b55686cc036f7a10a014
BenRStutzman <tysorx89@gmail.com>
2021-09-21 09:24:37 +0300

    make timeline look nice

--- about/history/index.php
+++ about/history/index.php
@@ -1,45 +1,190 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-    <?php
+  <?php
         $page_name = "History";
         include "{$_SERVER['DOCUMENT_ROOT']}/shared/_headTags.php";
     ?>
 </head>
+
 <body>
-    <?php include "{$_SERVER['DOCUMENT_ROOT']}/shared/_header.php"; ?>
-    <div class="page-heading">
-        <h1>History</h1>
-    </div>
-    <main>
-        <section>
-            <h2>Origins of the Diocese</h2>
-            <p>The creation of South Rwenzori Diocese is linked to the struggle for the Bamba-Bakonzo for self-administration. The yearning for an independent and separate district from Toro also crept into the church, which, like the local government, was headed by people who were not natives to the area. As parishes were created in the mountain areas, it became necessary for the church to have local people to help in spread the message of salvation. It is no wonder, therefore, that the first voices for a separate diocese came from Kitholhu parish, where pastoral work became difficult for a Kabarole–based bishop amidst the agitation for separate administration. Below is the chronology of events that led to the creation of South Rwenzori Diocese.</p>
-        </section>
-        <section>
-            <h2>Timeline</h2>
-            <ul>
-                <li>1973: The late Peter K. Muhyana becomes a member of the synod in Rwenzori Diocese and member of the Provincial Assembly of the Church of Uganda.</li>
-                <li>1974: The government grants Kasese a district status.
-                <li>1979: Muhyana, then treasurer and secretary of Kitojo parish in the Kasese archdeaconry, moots the idea of establishing an Anglican Diocese in Kasese in 1978-1979. The then Bishop of Rwenzori Diocese, Rt. Rev Yonasani Rwakaikara, falls ill, hampering Muhyana from advancing his dream with the Bishop. The illness disrupts two consecutive confirmation classes meant to be conducted by the Bishop, further strengthening the desire for an assistant bishop based in Kasese. Meanwhile, members of the Rwenzururu movement warn Bishop Rwakaikara against executing his duties in Kasese District, and direct church authorities in Kasese District not to remit any funds to Rwenzori Diocese. The Rwenzururu fighters periodically descend from the mountains, carrying away the church funds for 'safe custody' pending the creation of a separate diocese in Kasese. The fighers also stop Kasese church officials and synod representatives from attending the synod and other meetings in Rwenzori Diocese on the grounds that Rwenzururu was fighting for the church in Kasese District. The fighters kidnap and detain Kasese priests, catechists and missioners, including Rev. Yohana Masereka and Rev. Hosea Kiiza. The consequences are reduced church activities and services in Kasese archdeaconry and the displacement of the Kitojo vicar from his parish to Karambi, which enhance the vision to have a separate diocese. Muhyana successfully tables a motion calling for an assistant bishop in charge of Kasese, and a resolution is sent to the Kasese archdeaconry council. The archdeaconry council debates, adopts and forwards the petition to the diocesan synod of Rwenzori on 11th December 1979 for consideration. David Sebuhinja, one of the officials in the Rwenzori Diocese, supports the petition.</li>
-                <li>15.12.79: Muhyana returns to Kasese Archdeaconry and meets the Archdeacon, the Ven. Canon Stephen Mukirania, with whom he further shares his views about creating a new Diocese.</li>
-                <li>30.10.80: The issue of having an assistant bishop is once again tabled at the Kasese archdeaconry staff meeting.
-                <li>10.01.81: A general archdeaconry meeting appoints a joint committee comprising Rwenzururu representatives to establish peaceful negotiation between the Rwenzururu movement and the general Kasese populace. The respective sides send representatives to the negotiating table. Peter Muhyana chairs the church side, comprising Eriya Kambere, Zephaniah Bwambale and Christopher Kamabu. The Rwenzururu movement is represented by Yehosphat Kule (a.k.a School Band) and Daniel Masereka. Isaya Nzghundi and Rev. Can. Stephen Mukirania, by then the Archdeacon of Kasese Archdeconry, are appointed as advisors to the negotiations.</li>
-                <li>4.02.81: The first joint committee meeting decides to send a letter to Bishop Rwakaikara asking for a separate diocese before Bishop Rwakaikara is transferred to Rwenzori. The diocesan synod accepts the request and advises Kasese Archdeaconry to follow the established procedures of establishing a diocese.</li>
-                <li>25.05.81: The Kasese joint planning and development committee writes a reminder to the outgoing bishop, who refers the matter to his successor, Rt. Rev. Can. Eustace Kamanyire, for necessary action.</li>
-                <li>27.07.81: After his election, the Bishop-elect Kamanyire convenes a meeting of from Kasese archdeaconry and other parts of Rwenzori diocese to discuss the Kasese demand. Representatives from Kasese archdeaconry declare that the Bakonzo of Kasese will no longer submit to the leadership of the Rwenzori diocese.</li>
-                <li>6.8.81: The Kasese Archdeaconry council meets and plans to further push their demand at a provincial assembly meeting slated for 22-27.8.81. Kasese sends a three-person delegation led by Muhyana and Semu Kajurujuru and the late Christopher Kamabu, and another letter to the Archbishop of the Church of Uganda.</li>
-                <li>29.01.82: The Church of Uganda Archbishop, Syelevenus Wani, visits Kasese Archdeaconry.</li>
-                <li>31.1.82: The Archbishop tours Katwe and Bwera, meets the joint planning and development committee of Kasese Archdeaconry, and celebrates a holy communion service at Kisinga C.O.U.</li>
-                <li>1.02.82: The Archbishop meets Omusinga Irema and Ngoma Charles Wesley Mumbere at Kisinga Sub- County, where various socioeconomic concerns, including peace and the church, are addressed. The Archbishop also conducts a holy service at the All Saints Church Kilembe.</li>
-                <li>2.2.82: The Archbishop, assisted by the Bishop of Rwenzori Diocese, leads prayers at St. Paul’s Church of Uganda, by then headquarters of Kasese Archdeaconry, and tells the congregation to demand a diocese according to procedures.</li>
-                <li>5.02.82: The joint committee meets to discuss the Archbishop’s advice, and follows this with a series of meetings.</li>
-                </li>1984-2003: Bishop Masereka Kahangwa Zebedde</li>
-                <li>2003–2020: Bishop Nzerebende Jackson</li>
-                <li>2020–present: Bishop Baluku Nason. God willing, he will serve 12 years.</li>
-            </ul>
-        </section>
-    </main>
-    <?php include "{$_SERVER['DOCUMENT_ROOT']}/shared/_footer.php"; ?>
+  <?php include "{$_SERVER['DOCUMENT_ROOT']}/shared/_header.php"; ?>
+  <div class="page-heading">
+    <h1>History</h1>
+  </div>
+  <main>
+    <section>
+      <h2>Origins of the Diocese</h2>
+      <p>The creation of South Rwenzori Diocese is linked to the struggle for the Bamba-Bakonzo for
+        self-administration. The yearning for an independent and separate district from Toro also crept into the
+        church, which, like the local government, was headed by people who were not natives to the area. As
+        parishes were created in the mountain areas, it became necessary for the church to have local people to
+        help in spread the message of salvation. It is no wonder, therefore, that the first voices for a
+        separate diocese came from Kitholhu parish, where pastoral work became difficult for a Kabarole–based
+        bishop amidst the agitation for separate administration. Below is the chronology of events that led to
+        the creation of South Rwenzori Diocese.</p>
+    </section>
+    <section>
+      <h2>Timeline</h2>
+      <div class="timeline">
+        <div class="timeline-item">
+          <div class="timeline-item-content">
+            <h3>1973</h3>
+            <p>The late Peter K. Muhyana becomes a member of the synod in Rwenzori Diocese and member of the
+              Provincial Assembly of the Church of Uganda.</p>
+          </div>
+        </div>
+        <div class="timeline-item">
+          <div class="timeline-item-content">
+            <h3>1974</h3>
+            <p>The government grants Kasese a district status.</p>
+          </div>
+        </div>
+        <div class="timeline-item">
+          <div class="timeline-item-content">
+            <h3>1979</h3>
+            <p>Muhyana, then treasurer and secretary of Kitojo parish in the Kasese archdeaconry, moots the
+              idea of establishing an Anglican Diocese in Kasese in 1978-1979. The then Bishop of Rwenzori
+              Diocese, Rt. Rev Yonasani Rwakaikara, falls ill, hampering Muhyana from advancing his dream
+              with the Bishop. The illness disrupts two consecutive confirmation classes meant to be
+              conducted by the Bishop, further strengthening the desire for an assistant bishop based in
+              Kasese. Meanwhile, members of the Rwenzururu movement warn Bishop Rwakaikara against
+              executing his duties in Kasese District, and direct church authorities in Kasese District
+              not to remit any funds to Rwenzori Diocese. The Rwenzururu fighters periodically descend
+              from the mountains, carrying away the church funds for 'safe custody' pending the creation
+              of a separate diocese in Kasese. The fighers also stop Kasese church officials and synod
+              representatives from attending the synod and other meetings in Rwenzori Diocese on the
+              grounds that Rwenzururu was fighting for the church in Kasese District. The fighters kidnap
+              and detain Kasese priests, catechists and missioners, including Rev. Yohana Masereka and
+              Rev. Hosea Kiiza. The consequences are reduced church activities and services in Kasese
+              archdeaconry and the displacement of the Kitojo vicar from his parish to Karambi, which
+              enhance the vision to have a separate diocese. Muhyana successfully tables a motion calling
+              for an assistant bishop in charge of Kasese, and a resolution is sent to the Kasese
+              archdeaconry council. The archdeaconry council debates, adopts and forwards the petition to
+              the diocesan synod of Rwenzori on 11th December 1979 for consideration. David Sebuhinja, one
+              of the officials in the Rwenzori Diocese, supports the petition.</p>
+          </div>
+        </div>
+        <div class="timeline-item">
+          <div class="timeline-item-content">
+            <h3>15.12.79</h3>
+            <p>Muhyana returns to Kasese Archdeaconry and meets the Archdeacon, the Ven. Canon Stephen
+              Mukirania, with whom he further shares his views about creating a new Diocese.</p>
+          </div>
+        </div>
+        <div class="timeline-item">
+          <div class="timeline-item-content">
+            <h3>30.10.80</h3>
+            <p>The issue of having an assistant bishop is once again tabled at the Kasese archdeaconry staff
+              meeting.</p>
+          </div>
+        </div>
+        <div class="timeline-item">
+          <div class="timeline-item-content">
+            <h3>10.01.81</h3>
+            <p>A general archdeaconry meeting appoints a joint committee comprising Rwenzururu
+              representatives to establish peaceful negotiation between the Rwenzururu movement and the
+              general Kasese populace. The respective sides send representatives to the negotiating table.
+              Peter Muhyana chairs the church side, comprising Eriya Kambere, Zephaniah Bwambale and
+              Christopher Kamabu. The Rwenzururu movement is represented by Yehosphat Kule (a.k.a School
+              Band) and Daniel Masereka. Isaya Nzghundi and Rev. Can. Stephen Mukirania, by then the
+              Archdeacon of Kasese Archdeconry, are appointed as advisors to the negotiations.</p>
+          </div>
+        </div>
+        <div class="timeline-item">
+          <div class="timeline-item-content">
+            <h3>4.02.81</h3>
+            <p>The first joint committee meeting decides to send a letter to Bishop Rwakaikara asking for a
+              separate diocese before Bishop Rwakaikara is transferred to Rwenzori. The diocesan synod
+              accepts the request and advises Kasese Archdeaconry to follow the established procedures of
+              establishing a diocese.</p>
+          </div>
+        </div>
+        <div class="timeline-item">
+          <div class="timeline-item-content">
+            <h3>25.05.81</h3>
+            <p>The Kasese joint planning and development committee writes a reminder to the outgoing bishop,
+              who refers the matter to his successor, Rt. Rev. Can. Eustace Kamanyire, for necessary
+              action.</p>
+          </div>
+        </div>
+        <div class="timeline-item">
+          <div class="timeline-item-content">
+            <h3>27.07.81</h3>
+            <p>After his election, the Bishop-elect Kamanyire convenes a meeting of from Kasese archdeaconry
+              and other parts of Rwenzori diocese to discuss the Kasese demand. Representatives from
+              Kasese archdeaconry declare that the Bakonzo of Kasese will no longer submit to the
+              leadership of the Rwenzori diocese.</p>
+          </div>
+        </div>
+        <div class="timeline-item">
+          <div class="timeline-item-content">
+            <h3>6.8.81</h3>
+            <p>The Kasese Archdeaconry council meets and plans to further push their demand at a provincial
+              assembly meeting slated for 22-27.8.81. Kasese sends a three-person delegation led by
+              Muhyana and Semu Kajurujuru and the late Christopher Kamabu, and another letter to the
+              Archbishop of the Church of Uganda.</p>
+          </div>
+        </div>
+        <div class="timeline-item">
+          <div class="timeline-item-content">
+            <h3>29.01.82</h3>
+            <p>The Church of Uganda Archbishop, Syelevenus Wani, visits Kasese Archdeaconry.</p>
+          </div>
+        </div>
+        <div class="timeline-item">
+          <div class="timeline-item-content">
+            <h3>31.1.82</h3>
+            <p>The Archbishop tours Katwe and Bwera, meets the joint planning and development committee of
+              Kasese Archdeaconry, and celebrates a holy communion service at Kisinga C.O.U.</p>
+          </div>
+        </div>
+        <div class="timeline-item">
+          <div class="timeline-item-content">
+            <h3>1.02.82</h3>
+            <p>The Archbishop meets Omusinga Irema and Ngoma Charles Wesley Mumbere at Kisinga Sub- County,
+              where various socioeconomic concerns, including peace and the church, are addressed. The
+              Archbishop also conducts a holy service at the All Saints Church Kilembe.</p>
+          </div>
+        </div>
+        <div class="timeline-item">
+          <div class="timeline-item-content">
+            <h3>2.2.82</h3>
+            <p>The Archbishop, assisted by the Bishop of Rwenzori Diocese, leads prayers at St. Paul’s
+              Church of Uganda, by then headquarters of Kasese Archdeaconry, and tells the congregation to
+              demand a diocese according to procedures.</p>
+          </div>
+        </div>
+        <div class="timeline-item">
+          <div class="timeline-item-content">
+            <h3>5.02.82</h3>
+            <p>The joint committee meets to discuss the Archbishop’s advice, and follows this with a series
+              of meetings.</p>
+          </div>
+        </div>
+        <div class="timeline-item">
+          <div class="timeline-item-content">
+            <h3>1984-2003</h3>
+            <p>Bishop Masereka Kahangwa Zebedde</p>
+          </div>
+        </div>
+        <div class="timeline-item">
+          <div class="timeline-item-content">
+            <h3>2003–2020</h3>
+            <p>Bishop Nzerebende Jackson</p>
+          </div>
+        </div>
+        <div class="timeline-item">
+          <div class="timeline-item-content">
+            <h3>2020–present</h3>
+            <p>Bishop Baluku Nason. God willing, he will serve 12 years.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+  <?php include "{$_SERVER['DOCUMENT_ROOT']}/shared/_footer.php"; ?>
 </body>
+
 </html>
